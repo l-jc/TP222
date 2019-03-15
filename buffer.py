64a48b7a793ba2975e040ff708b4f35ee4cb7002
Jiacheng Luo <jiacheng_luo@yahoo.com>
2019-03-15 15:27:06 -0700

    framework update

---
+++ buffer.py
@@ -0,0 +1,22 @@
+
+class Buffer:
+    def __init__(self):
+        # start and end seq number
+        pass
+
+    def get(self, size: int) -> bytes:
+        pass
+
+    def insert(self):
+        pass
+
+
+
+class ShrimpBytes:
+    def __init__(self):
+        # sequence number range.
+        pass
+
+    def replace(self, start: int, size: int, data: bytes):
+        pass
+
