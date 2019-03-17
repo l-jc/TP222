d2c63ca01bdd43bbe6549d1c035ddf75b5347e41
mcheng916 <mcheng916@gmail.com>
2019-03-17 14:58:46 -0700

    Update utils.py
    Utils

--- utils.py
+++ utils.py
@@ -1,18 +1,31 @@
-
+import time
+import numpy as np
+import pickle
 class Generator:
     """
     this is part of application level.
     don't have to worry about until we implement transport level.
     """
     def __init__(self):
-        pass
+        # Check if it's time to generate another iframe/pframe
+        self.startTime = time.time()
+        self.iFrameList = []
+        self.pFrameList = []
+        
 
     def get_iframe(self):
-        pass
+        res = np.random.randint(10,240, size=(640, 640,3))
+        iFrameList.append(res)
+        return res
 
     def get_pframe(self):
-        pass
+        res = np.random.randint(0,11, size=(80, 80,3))
+        pFrameList.append(res)
+        return res
 
     def store(self):
-        pass
+        with open('iframePKL.pkl', 'wb') as f:
+            pkl.dump(iFrameList, f)
+        with open('pframePKL.pkl', 'wb') as f:
+            pkl.dump(pFrameList, f)
 
