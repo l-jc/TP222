e571b76923aa573fdd0f69b2873be471480b0cd5
mcheng916 <mcheng916@gmail.com>
2019-03-19 14:45:49 -0700

    AA

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
 import pickle as pkl
+import sys
 class Generator:
     """
     this is part of application level.
@@ -10,22 +11,42 @@ class Generator:
         # Check if it's time to generate another iframe/pframe
         self.iFrameList = []
         self.pFrameList = []
-        
+        self.CONST_dtype = 'int64'
+        self.CONST_pFrameSize = 153633
+        self.pFrameShape = (80, 80, 3)
+        self.iFrameShape = (640, 640, 3)
 
     def get_iframe(self):
-        res = np.random.randint(10,240, size=(640, 640,3))
+        res = np.random.randint(10,240, size=self.iFrameShape)
         self.iFrameList.append(res)
-        return res
+        byteRes = res.tobytes()
+        return byteRes
 
     def get_pframe(self):
-        res = np.random.randint(0,11, size=(80, 80,3))
+        res = np.random.randint(0,11, size=self.pFrameShape)
         self.pFrameList.append(res)
+        byteRes = res.tobytes()
+        return byteRes
+
+    def byteToFrame(self,frameInput):
+        if sys.getsizeof(frameInput) <= self.CONST_pFrameSize:
+            res = np.frombuffer(frameInput,dtype=self.dtype).reshape(self.pFrameShape)
+        else:
+            res = np.frombuffer(frameInput,dtype=self.dtype).reshape(self.iFrameShape)
         return res
-        
+    
+    # # integer to byte
+    # # https://www.devdungeon.com/content/working-binary-data-python#int-to-bytes
+    # def conv2Bytes(self, frame):
+    #     res = b''
+    #     for i in frame:
+    #         for j in i:
+    #             for k in j:
+    #                 res += k.to_bytes(1, byteorder='big', signed=False)
+    #     return res
 
     def store(self):
         with open('iframePKL.pkl', 'wb') as f:
             pkl.dump(self.iFrameList, f)
         with open('pframePKL.pkl', 'wb') as f:
             pkl.dump(self.pFrameList, f)
-
